816313e62089164b45001e28bf3ea0d52a981b4e
AndrMar1939 <advo737@gmail.com>
2023-01-10 23:30:42 +0200

    three exercises has been finished

--- JS_HW5/js/script.js
+++ JS_HW5/js/script.js
@@ -1,103 +1,57 @@
-// HW 4
-// student arr
-const studentArr = [{
-        name: 'Сергей',
-        surname: 'Войлов',
-        ratingPoint: 1000,
-        schoolPoint: 1000,
-        course: 2,
-    },
-    {
-        name: 'Татьяна',
-        surname: 'Коваленко',
-        ratingPoint: 880,
-        schoolPoint: 700,
-        course: 1,
-    },
-    {
-        name: 'Анна',
-        surname: 'Кугир',
-        ratingPoint: 1430,
-        schoolPoint: 1200,
-        course: 3,
-    },
-    {
-        name: 'Станислав',
-        surname: 'Щелоков',
-        ratingPoint: 1130,
-        schoolPoint: 1060,
-        course: 2,
-    },
-    {
-        name: 'Денис',
-        surname: 'Хрущ',
-        ratingPoint: 1000,
-        schoolPoint: 990,
-        course: 4,
-    },
-    {
-        name: 'Татьяна',
-        surname: 'Капустник',
-        ratingPoint: 650,
-        schoolPoint: 500,
-        course: 3,
-    },
-    {
-        name: 'Максим',
-        surname: 'Меженский',
-        ratingPoint: 990,
-        schoolPoint: 1100,
-        course: 1,
-    },
-    {
-        name: 'Денис',
-        surname: 'Марченко',
-        ratingPoint: 570,
-        schoolPoint: 1300,
-        course: 4,
-    },
-    {
-        name: 'Антон',
-        surname: 'Завадский',
-        ratingPoint: 1090,
-        schoolPoint: 1010,
-        course: 3
-    },
-    {
-        name: 'Игорь',
-        surname: 'Куштым',
-        ratingPoint: 870,
-        schoolPoint: 790,
-        course: 1,
-    },
-    {
-        name: 'Инна',
-        surname: 'Скакунова',
-        ratingPoint: 1560,
-        schoolPoint: 200,
-        course: 2,
-    },
-];
+// HW 5
+// ____________________________exercise 1
 
-// ______________________________________________________________________________________________________________________exercise 1_________________________________________________
-
-class Student {
-    
-    constructor(enrollee) {
-        this.id = Student.ID++;
-        for (let ch in enrollee) {
-            this[ch] = enrollee[ch];
-        }
-    }
-
-    static ID=1;
-    
-
+function count () {
+    let number = null;
+    return (num) => number += num;
 }
+const counter = count();
 
+console.log(counter(3));
+console.log(counter(7));
+console.log(counter(344));
 
+// ____________________________exercise 2
 
-let stud = new Student(studentArr[0]);
-let stud1 = new Student(studentArr[1]);
-console.log(stud);
-console.log(stud1);
+function getArr () {
+    let arr = [];
+    return function (arg) {
+        if (!arg) {
+            return arr = [];    
+        }
+        arr.push(arg);
+        return arr;
+    }
+}
+const getUpdatedArr = getArr();
+
+console.log(getUpdatedArr(5));
+console.log(getUpdatedArr([1,2,3]));
+console.log(getUpdatedArr('Andrii'));
+console.log(getUpdatedArr());
+console.log(getUpdatedArr('Andrii'));
+console.log(getUpdatedArr(3));
+
+// ____________________________exercise 3
+
+function foo () {
+    let firstTime = new Date;
+    let counter = 0;
+
+    return function () {        
+        let secondTime = new Date;
+        let time = Math.trunc((secondTime - firstTime) / 1000);
+        
+        firstTime = secondTime;       
+        counter++;
+
+        if (counter === 1) {
+           return "Enabled"; 
+        }
+        return time;       
+    }
+}
+let getTime = foo();
+
+// ____________________________exercise 4
+
